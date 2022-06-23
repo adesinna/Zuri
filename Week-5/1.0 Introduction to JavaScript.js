2672a6c9c6f9336ad98934e653d2ece46a7f5015
Adesina Aladesae <101404177+adesinna@users.noreply.github.com>
2022-06-23 14:28:56 +0100

    Update 1.0 Introduction to JavaScript.js

--- Week-5/1.0 Introduction to JavaScript.js
+++ Week-5/1.0 Introduction to JavaScript.js
@@ -5,3 +5,18 @@ let height = Number(window.prompt('Enter your height'));
 let country = window.prompt('Enter your country');
 
 console.log(`Your name is ${name}, age is ${height} and you are from ${country}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
